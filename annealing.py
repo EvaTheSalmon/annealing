e1a4f3fb77e5c751b853d7002c3e02f58622b848
Oleg <18466855+EvaTheSalmon@users.noreply.github.com>
2021-04-18 03:16:43 +0300

    Refactoring and adding new flags

--- annealing.py
+++ annealing.py
@@ -9,7 +9,6 @@ options.mode.chained_assignment = None  # default='warn'
 def datetime_parse(date, offset):
     i = 0
     for string in date['Дата Время']:
-
         time_point = string[11:]
         hour_point = float(time_point[:2])
         minute_point = float(time_point[3:5])
@@ -23,8 +22,8 @@ def datetime_parse(date, offset):
         millisecond_offset = float(time_offset[9:12])
 
         date.at[i, 'Дата Время'] = \
-            (hour_point-hour_offset)*3600 + (minute_point-minute_offset)*60 + \
-            second_point-second_offset + (millisecond_point-millisecond_offset)*0.001
+            (hour_point - hour_offset) * 3600 + (minute_point - minute_offset) * 60 + \
+            second_point - second_offset + (millisecond_point - millisecond_offset) * 0.001
         i += 1
 
 
@@ -48,19 +47,15 @@ def yes_no_question(question) -> bool:
         sys.stdout.write("Please respond with 'yes' or 'no'")
 
 
-def write_processes(path) -> None:
-    try:
-        p = Path(path)
-    except IOError:
-        print("Could not read:", path)
-        return
-
-    with open(path, encoding="utf8") as fp:
+def parse_processes(input, output) -> None:
+    p = Path(input)
+    with open(input, encoding="utf8") as fp:
         data = read_csv(fp, sep=';')
         data = data.stack().str.replace(',', '.').unstack()
         data = data.fillna(0)
 
-        dir_name = 'process_' + get_process_date(data) + '_data'
+        dir_name = 'process_' + get_process_date(
+            data) + '_data'  # todo: output processes should be sorted by their date
         output_dir = str(p.parent) + "\\" + str(dir_name)
 
         try:
@@ -90,14 +85,14 @@ def write_processes(path) -> None:
                 if i == 1: process = process.reset_index(drop=True)  # first may be with zeros at the beginning
 
                 datetime_parse(process, offset)
-                process.to_csv(output_dir+'\\process_' + str(i) + '_started_at_' +
+                process.to_csv(output_dir + '\\process_' + str(i) + '_started_at_' +
                                str(offset[11:][:-4]).replace(":", ".") + '.csv', index=False)
             else:
                 process = data.iloc[first_non_zero:]
 
                 datetime_parse(process, offset)
 
-                process.to_csv(output_dir+'\\process_' + str(i) + '_started_at_' +
+                process.to_csv(output_dir + '\\process_' + str(i) + '_started_at_' +
                                str(offset[11:][:-4]).replace(":", ".") + '.csv', index=False)
                 break
             i += 1
@@ -110,16 +105,85 @@ def write_processes(path) -> None:
         sys.stdout.write('\n' + str(i) + ' files writen to ' + output_dir)
 
 
-def main() -> None:
+def concat_processes(input, output) -> None:
+    print()
 
-    parser = argparse.ArgumentParser(usage="%(prog)s [options]")
+
+def shift_processes(input, output) -> None:
+    print()
+
+
+def check_output(output_path) -> bool:
+    if Path(output_path).is_dir():
+        return True
+    if not Path(output_path).is_dir():
+        if yes_no_question('Provided path not exist and will be created'):
+            Path(output_path).mkdir(parents=True)
+            return True
+        else:
+            print("Stopped by user")
+            return False
+    else:
+        return False
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(usage="%(prog)s [options]", description='Tool to process data from\
+     annealing machine STE RTA100')
     parser.add_argument(
-        "--path", "-p",
+        "path", metavar='/path/to/your.file', type=str, nargs='+',
+        help="path to '.csv' files to extract data to work with"
+    )
+
+    parser.add_argument(
+        "--parse", "-p",
+        action="store_true",
+        help="search data for processes and split them in separate files\
+             with time offsetting to zero (used by default if no flags specified)"
+    )
+
+    parser.add_argument(
+        "--output", "-o",
         action="store",
-        help="Path to '.csv' file with data to parse processes from"
+        help="custom output path to save results. if not provided input file dir is used by default"
+    )
+
+    parser.add_argument(
+        "--concat", "-c",
+        action="store_true",
+        help="concat processes in one file with time sensitivity"
+    )
+
+    parser.add_argument(
+        "--shift", "-s",
+        action="store_true",
+        help="offset time of processes on certain amount in seconds"
     )
 
     args = parser.parse_args()
 
-    path = args.path
-    write_processes(path)
+    path = args.path[0]
+    print(path)
+
+    if not Path(path).is_file():
+        print("Could not read:", path)
+        return
+
+    parse = args.parse
+    output = args.output
+    concat = args.concat
+    shift = args.shift
+
+    if output:
+        if not check_output(output):
+            print('Output path can\'t be used')
+            return
+
+    if not (parse and output and concat and shift):  # default action with no flags provided
+        parse_processes(path, output)
+    if parse:
+        parse_processes(path, output)
+    if concat:
+        concat_processes(path, output)
+    if shift:
+        shift_processes(path, output)
