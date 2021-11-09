a78e69e70edf56e2d849cb3da90e9e4f64684884
Oleg <18466855+EvaTheSalmon@users.noreply.github.com>
2021-11-10 01:06:06 +0300

    first commit without extra packages

--- annealing/__main__.py
+++ annealing/__main__.py
@@ -1,16 +1,60 @@
 import argparse
 import sys
-from pandas import options
+from io import open
+import csv
 
-import function_lib
-from shift import shift_processes
-from concat import concat_processes
-from parse import parse_processes
-from function_lib import check_input
+__version__ = 1.2
 
-__version__ = 1.1
 
-options.mode.chained_assignment = None  # default='warn'
+class Process:
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def _parse(path: str):
+        with open(path, "r", encoding="utf-8-sig") as processfile:
+            process = csv.reader(processfile, delimiter=";")
+
+            headers = next(process)
+            date_id = headers.index('Дата Время')
+            temp_id = headers.index('Температура')
+            powr_id = headers.index('Мощность')
+
+            heating_process = []
+            heating_process += ['Time', 'Temp', 'Power']
+
+            heating_flag = 0
+            cooling_flag = 0
+
+            for row in process:
+
+                powr = row[powr_id].split(",", 1)[0]
+                temp = row[temp_id].split(",", 1)[0]
+                date = row[date_id].split(" ", 1)[1]
+
+                if powr != 0 and heating_flag == 0:
+                    heating_flag = 1
+                    cooling_flag = 0
+
+                    heating_process += [date, temp, date]
+
+                    if heating_process:
+                        with open('names.csv', 'w', newline='\r\n', encoding='utf-8-sig') as csvfile:
+                            writer = csv.writer(csvfile)
+                            # todo better to write line on every read without even heating process stored
+
+                if heating_flag == 1:
+                    heating_process += [date, temp, date]
+
+                if heating_flag == 1 and powr == 0:
+                    cooling_flag = 1
+                    heating_flag = 0
+
+                    heating_process += [date, temp, date]
+
+                if cooling_flag == 1:
+                    heating_process += [date, temp, date]
 
 
 def main(self) -> None:
@@ -21,77 +65,12 @@ def main(self) -> None:
         help="input path to '.csv' files to extract data to work with"
     )
 
-    parser.add_argument(
-        "--parse", "-p",
-        action="store_true",
-        help="search data for processes and split them in separate files\
-             with time offsetting to zero (used by default if no flags specified)"
-    )
-
-    # parser.add_argument(
-    #     "--output", "-o",
-    #     action="store",
-    #     help="custom output path to save results. if not provided input file dir is used by default"
-    # )
-
-    # parser.add_argument(
-    #     "--concat", "-c",
-    #     action="store_true",
-    #     help="concat processes in one file with time sensitivity"
-    # )
-
-    parser.add_argument(
-        "--yes", "-y",
-        action="store_true",
-        help="agree and skip all data check yes/no questions such as rewrite data alert and others"
-    )
-
-    parser.add_argument(
-        "--version", "-v",
-        action="version",
-        version='%(prog)s ' + str(__version__)
-    )
-
-    # parser.add_argument(
-    #     "--shift", "-s",
-    #     action="store",
-    #     type=float,
-    #     nargs=1,
-    #     help="offset time of processes on certain amount in seconds"
-    # )
-
     args = parser.parse_args()
+    input_file = args.path
+    process = Process()
 
-    input_path = args.path
-
-    function_lib.yes = False
-    function_lib.yes = args.yes
-
-    parse = args.parse
-    # output_path = args.output
-    output_path = ""
-    # concat = args.concat
-    concat = False
-    # shift = args.shift
-    shift = None
-
-    # default action with no flags provided
-    if parse is False and concat is False and shift is None:
-        for eachpath in input_path:
-            if check_input(eachpath):
-                parse_processes(eachpath, output_path)
-    elif parse:
-        for eachpath in input_path:
-            if check_input(eachpath):
-                parse_processes(eachpath, output_path)
-
-    if concat:
-        concat_processes(input_path, output_path)
-
-    if shift:
-        for eachpath in input_path:
-            if check_input(eachpath):
-                shift_processes(eachpath, output_path, shift[0])
+    for eachfile in input_file:
+        process._parse(eachfile)
 
 
 if __name__ == '__main__':
